7ca3db9457bb07eaf0fc833e0b8d41ef675d00a2
steve <551619855@qq.com>
2017-06-02 08:20:05 +0800

    add my ekf...

--- src/IntegratingIMUDemo.cpp
+++ src/IntegratingIMUDemo.cpp
@@ -15,32 +15,46 @@
 #include "sophus/se3.h"
 
 
-
-int main()
-{
-   std::string dir_name = "/home/steve/tmp/test/51/" ;
-
-   CSVReader imuReader(dir_name+"sim_imu.csv");
-   CSVReader zuptReader(dir_name+"sim_zupt.csv");
-
-   // Load data
-
-   Eigen::MatrixXd imu_data(imuReader.GetMatrix().GetRows(),imuReader.GetMatrix().GetCols());
-   Eigen::MatrixXd zupt_data(zuptReader.GetMatrix().GetRows(),zuptReader.GetMatrix().GetCols());
-
-   for(int i(0);i<imuReader.GetMatrix().GetRows();++i)
-   {
-      for(int j(0);j<imuReader.GetMatrix().GetCols();++j)
-      {
-         imu_data(i,j) = *(imuReader.GetMatrix()(i,j));
-      }
-      zupt_data(i,0) = *(zuptReader.GetMatrix()(i,0));
-   }
+#include "MYEKF.h"
+#include "SettingPara.h"
 
 
-   // Prepare State value
+int main() {
+    std::string dir_name = "/home/steve/tmp/test/51/";
 
-   
+    CSVReader imuReader(dir_name + "sim_imu.csv");
+    CSVReader zuptReader(dir_name + "sim_zupt.csv");
+
+    // Load data
+
+    Eigen::MatrixXd imu_data(imuReader.GetMatrix().GetRows(), imuReader.GetMatrix().GetCols());
+    Eigen::MatrixXd zupt_data(zuptReader.GetMatrix().GetRows(), zuptReader.GetMatrix().GetCols());
+
+    for (int i(0); i < imuReader.GetMatrix().GetRows(); ++i) {
+        for (int j(0); j < imuReader.GetMatrix().GetCols(); ++j) {
+            imu_data(i, j) = *(imuReader.GetMatrix()(i, j));
+        }
+        zupt_data(i, 0) = *(zuptReader.GetMatrix()(i, 0));
+    }
+
+
+    /**
+     * MyEKF
+     */
+
+
+    SettingPara init_para(true);
+
+    init_para.init_pos1_ = Eigen::Vector3d(0.0, 0.0, 0.0);
+//    init_para.init_heading1_ = 0.0 + 20 / 180.0 * M_PI;
+    init_para.init_heading1_ = M_PI / 2.0;
+
+    init_para.Ts_ = 1.0 / 128.0;
+
+    MyEkf myekf(init_para);
+
+    myekf.InitNavEq(imu_data.block(0, 1, 20, 6));
+
 
 
 
