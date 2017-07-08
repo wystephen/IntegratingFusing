d79f59d93aed6994a72cbe1107648885f3940490
steve <551619855@qq.com>
2017-07-08 15:35:29 +0800

    so

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -2,3 +2,59 @@
 // Created by steve on 17-7-8.
 //
 
+#include <iostream>
+#include <cmath>
+
+
+#include "CSVReader.h"
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+#include <EKFSimple.h>
+#include <EKFR.h>
+
+#include "sophus/so3.h"
+#include "sophus/se3.h"
+
+
+#include "MYEKF.h"
+#include "EKF.hpp"
+#include "SettingPara.h"
+#include "EKFEigen.h"
+//#include <pangolin/pangolin.h>
+
+int main()
+{
+    // INPUT FILE
+    std::string dir_name = "/home/steve/Code/Mini_IMU/Scripts/IMUWB/93/";
+
+    CppExtent::CSVReader imuReader(dir_name + "imu.txt");
+    CppExtent::CSVReader zuptReader(dir_name + "sim_zupt.csv");
+
+    auto imuM = imuReader.GetMatrix();
+    auto zuptM = zuptReader.GetMatrix();
+
+    Eigen::MatrixXd imu_data(imuM.GetRows(), imuM.GetCols());
+    Eigen::MatrixXd zupt_data(zuptM.GetRows(), zuptM.GetCols());
+
+
+
+    for (int i(0); i < imuM.GetRows(); ++i) {
+        for (int j(0); j < imuM.GetCols(); ++j) {
+            imu_data(i, j) = double(*(imuM(i, j)));
+            if (0 < j < 4) {
+                imu_data(i, j) *= 9.81;
+            } else if (4 <= j < 7) {
+                imu_data(i,j) *= (M_PI/180.0);
+            }
+        }
+
+        zupt_data(i, 0) = *(zuptM(i, 0));
+    }
+
+
+
+    // OUTPUT FILE
+    std::ofstream out_file("./ResultData/out_result.txt");
+}
