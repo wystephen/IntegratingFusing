d4969135534f198ef3d09bf9e5b1b647fb1257d9
steve <551619855@qq.com>
2017-07-24 13:52:23 +0800

    error....

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -28,7 +28,7 @@ int main() {
 
     // Special initial value
 
-    Eigen::MatrixXd K(9,3);
+    Eigen::MatrixXd K(9, 3);
     K.setIdentity();
 
 
@@ -97,26 +97,26 @@ int main() {
             -sp, sr * cp, cr * cp;
     C_prev = C;
 
-    Eigen::MatrixXd heading(1,data_size);
-    heading(0,0) = yaw;
+    Eigen::MatrixXd heading(1, data_size);
+    heading(0, 0) = yaw;
 
 
-    Eigen::MatrixXd acc_n(data_size,3);// acc in navigation frame
-    acc_n.block(0,0,1,3) = (C*acc_s.block(0,0,1,3).transpose()).transpose();
+    Eigen::MatrixXd acc_n(data_size, 3);// acc in navigation frame
+    acc_n.block(0, 0, 1, 3) = (C * acc_s.block(0, 0, 1, 3).transpose()).transpose();
 
 
-    Eigen::MatrixXd vel_n(data_size,3);
+    Eigen::MatrixXd vel_n(data_size, 3);
     vel_n.setZero();
 
-    Eigen::MatrixXd pose_n(data_size,3);
+    Eigen::MatrixXd pose_n(data_size, 3);
     pose_n.setZero();
 
-    Eigen::MatrixXd distance(data_size,1);
+    Eigen::MatrixXd distance(data_size, 1);
     distance.setZero();
 
 
     // Error covariance
-    Eigen::MatrixXd P(9,9);
+    Eigen::MatrixXd P(9, 9);
     P.setZero();
 
     // Sigma noise
@@ -124,92 +124,100 @@ int main() {
     double sigma_a = 1e-2;
 
     // ZUPT
-    Eigen::MatrixXd H(9,9);
+    Eigen::MatrixXd H(9, 9);
 
-    for(int i(6);i<9;++i)
-    {
-        H(i,i) = 1.0;
+    for (int i(6); i < 9; ++i) {
+        H(i, i) = 1.0;
     }
 
     double sigma_v = 1e-2;
 
-    Eigen::MatrixXd R(3,3);
-    for(int i(0);i<3;++i)
-    {
-        R(i,i)  = sigma_v*sigma_v;
+    Eigen::MatrixXd R(3, 3);
+    for (int i(0); i < 3; ++i) {
+        R(i, i) = sigma_v * sigma_v;
     }
 
     double gyro_threshold = 0.6;
 
-    for(int t(1);t<data_size;++t)
-    {
-        double dt = timestamp(t)-timestamp(t-1);
+    for (int t(1); t < data_size; ++t) {
+        double dt = timestamp(t) - timestamp(t - 1);
 
         //Skew-symmetric matrix for angular rates
 
-        Eigen::Vector3d gyro_s1 = gyro_s.block(t,0,1,3).transpose();
+        Eigen::Vector3d gyro_s1 = gyro_s.block(t, 0, 1, 3).transpose();
 
         Eigen::Matrix3d ang_rate_matrix;
 
-        ang_rate_matrix<< 0.0,-gyro_s(2),gyro_s1(1),
-        gyro_s1(2),0.0,-gyro_s1(0),
-        -gyro_s1(1),gyro_s1(0),0.0;
+        ang_rate_matrix << 0.0, -gyro_s(2), gyro_s1(1),
+                gyro_s1(2), 0.0, -gyro_s1(0),
+                -gyro_s1(1), gyro_s1(0), 0.0;
 
-        C = C_prev*((2*Eigen::Matrix3d::Identity()+(dt * ang_rate_matrix ))*
-                (2.0*Eigen::Matrix3d::Identity()-(dt * ang_rate_matrix)).inverse());
+        C = C_prev * ((2 * Eigen::Matrix3d::Identity() + (dt * ang_rate_matrix)) *
+                      (2.0 * Eigen::Matrix3d::Identity() - (dt * ang_rate_matrix)).inverse());
 
 //        acc_n.block()
 
-        acc_n.block(t,0,1,3) = (C*acc_s.block(t,0,1,3).transpose().eval()).transpose();
+        acc_n.block(t, 0, 1, 3) = (C * acc_s.block(t, 0, 1, 3).transpose().eval()).transpose();
 
-        vel_n.block(t,0,1,3) = vel_n.block(t-1,0,1,3) +
-                dt / 2.0 * ((acc_n.block(t,0,1,3)-Eigen::Vector3d(0,0,g).transpose())+
-                        (acc_n.block(t-1,0,1,3)-Eigen::Vector3d(0,0,g).transpose()));
+        vel_n.block(t, 0, 1, 3) = vel_n.block(t - 1, 0, 1, 3) +
+                                  dt / 2.0 * ((acc_n.block(t, 0, 1, 3) - Eigen::Vector3d(0, 0, g).transpose()) +
+                                              (acc_n.block(t - 1, 0, 1, 3) - Eigen::Vector3d(0, 0, g).transpose()));
 
-        pose_n.block(t,0,1,3) = pose_n.block(t-1,0,1,3) +
-                dt / 2.0 * (vel_n.block(t,0,1,3)+vel_n.block(t-1,0,1,3));
+        pose_n.block(t, 0, 1, 3) = pose_n.block(t - 1, 0, 1, 3) +
+                                   dt / 2.0 * (vel_n.block(t, 0, 1, 3) + vel_n.block(t - 1, 0, 1, 3));
 
 
         Eigen::Matrix3d S;
-        S<< 0.0,-acc_n(t,2),acc_n(t,1),
-        acc_n(t,2),0.0,-acc_n(t,0),
-        -acc_n(t,1),acc_n(t,0),0.0;
+        S << 0.0, -acc_n(t, 2), acc_n(t, 1),
+                acc_n(t, 2), 0.0, -acc_n(t, 0),
+                -acc_n(t, 1), acc_n(t, 0), 0.0;
 
-        Eigen::MatrixXd F(9,9);
+        Eigen::MatrixXd F(9, 9);
         F.setZero();
 
-        F.block(0,0,3,3) = Eigen::Matrix3d::Identity();
-        F.block(3,3,3,3) = Eigen::Matrix3d::Identity();
-        F.block(6,6,3,3) = Eigen::Matrix3d::Identity();
+        F.block(0, 0, 3, 3) = Eigen::Matrix3d::Identity();
+        F.block(3, 3, 3, 3) = Eigen::Matrix3d::Identity();
+        F.block(6, 6, 3, 3) = Eigen::Matrix3d::Identity();
 
-        F.block(3,6,3,3) = dt * Eigen::Matrix3d::Identity();
-        F.block(6,3,3,3) = -dt * S;
+        F.block(3, 6, 3, 3) = dt * Eigen::Matrix3d::Identity();
+        F.block(6, 3, 3, 3) = -dt * S;
 
-        Eigen::MatrixXd Q(9,9);
+        Eigen::MatrixXd Q(9, 9);
         Q.setZero();
 
-        Q.block(0,0,3,3) = sigma_omega*sigma_omega*dt*Eigen::Matrix3d::Identity();
-        Q.block(6, 6,3,3) = sigma_a*sigma_a*dt*Eigen::Matrix3d::Identity();
+        Q.block(0, 0, 3, 3) = sigma_omega * sigma_omega * dt * Eigen::Matrix3d::Identity();
+        Q.block(6, 6, 3, 3) = sigma_a * sigma_a * dt * Eigen::Matrix3d::Identity();
 
         // propagate the error covariance matrix
-        P = F*P*F.transpose().eval() + Q;
+        P = F * P * F.transpose().eval() + Q;
 
         // Zero-velocity updates
-        if( gyro_s.block(t,0,1,3).norm() < gyro_threshold)
-        {
-            K = (P*H.transpose())*(H*P*H.transpose()+R);
+        if (gyro_s.block(t, 0, 1, 3).norm() < gyro_threshold) {
+            K = (P * H.transpose()) * (H * P * H.transpose() + R);
 
-            auto delta_x = K * vel_n.block(t,0,1,3).transpose();
+            auto delta_x = K * vel_n.block(t, 0, 1, 3).transpose();
             delta_x = delta_x.transpose().eval();
+
+
+            // update the error covariance matrix
+            P = (Eigen::Matrix<double, 9, 9>::Identity() - K * H) * P;
+
+            Eigen::Vector3d attitude_error = delta_x.block(0, 0, 3, 1);
+            Eigen::Vector3d pos_error = delta_x.block(3, 0, 3, 1);
+            Eigen::Vector3d vel_error = delta_x.block(6, 0, 3, 1);
+
+            Eigen::Matrix3d ang_matrix;
+            ang_matrix << 0.0, -attitude_error(2), attitude_error(1),
+                    attitude_error(2), 0.0, -attitude_error(0),
+                    -attitude_error(1), attitude_error(0), 0.0;
+            ang_matrix *= -1.0;
+
+            C = (2 * Eigen::Matrix3d::Identity() + ang_matrix) *
+                (2.0 * Eigen::Matrix3d::Identity() - ang_matrix).inverse()
+                * C;
+
+            vel_n.block()
         }
-
-
-
-
-
-
-
-
 
 
     }
