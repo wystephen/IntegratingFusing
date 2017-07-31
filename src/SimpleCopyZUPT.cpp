e3c9552a4e7b05f0bd2d57ac24dc7975154adca0
steve <551619855@qq.com>
2017-07-31 09:41:29 +0800

    Some problem before calculating the matrix F.

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -282,6 +282,7 @@ int main() {
             if (std::isnan(C.sum())) {
                 std::cout << C << "\n" << "t: " << t << "\nline:"
                           << __LINE__ << std::endl;
+                std::cout << "delta x :" <<  delta_x.block(t,0,1,3);
                 C = C_prev;
             }
 
