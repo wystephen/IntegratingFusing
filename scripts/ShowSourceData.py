2268efa50a43af54d70ae0dd044b580049165615
steve <551619855@qq.com>
2017-08-02 09:56:31 +0800

    Some problem before calculating the matrix F.

--- scripts/ShowSourceData.py
+++ scripts/ShowSourceData.py
@@ -4,24 +4,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 data = np.loadtxt("/home/steve/Code/Mini-IMU/Scripts/IMUWB/46/imu.txt",
                   delimiter=',')
 
 plt.figure()
-print(data[:,1:4].shape)
-print(np.mean(data[:20,1:4]*9.81,axis=0))
-print(np.linalg.norm( np.mean(data[:20,1:4]*9.81,axis=0)))
-print(np.mean(data[:20,1:4],axis=0))
-print(np.linalg.norm( np.mean(data[:20,1:4],axis=0)))
-plt.plot(np.linalg.norm(data[:,1:4],axis=1)*9.81,'r')
+print(data[:, 1:4].shape)
+print(np.mean(data[:20, 1:4] * 9.81, axis=0))
+print(np.linalg.norm(np.mean(data[:20, 1:4] * 9.81, axis=0)))
+print(np.mean(data[:20, 1:4], axis=0))
+print(np.linalg.norm(np.mean(data[:20, 1:4], axis=0)))
+plt.plot(np.linalg.norm(data[:, 1:4], axis=1) * 9.81, 'r')
 plt.figure()
-plt.plot(np.linalg.norm(data[:,4:7],axis=1)*np.pi/180.0,'g')
+plt.plot(np.linalg.norm(data[:, 4:7], axis=1) * np.pi / 180.0, 'g')
 plt.show()
 
-tmp_data = data[:,:7]
+tmp_data = data[:, :7]
 print(tmp_data.shape)
-tmp_data[:,0]-=tmp_data[0,0]
-tmp_data[:,1:4] *= 9.81
-tmp_data[:,4:7] *= (3.1415/180.0)
-np.savetxt("test.csv",tmp_data,delimiter=';')
+tmp_data[:, 0] -= tmp_data[0, 0]
+tmp_data[:, 1:4] *= 9.81
+tmp_data[:, 4:7] *= (3.1415 / 180.0)
+np.savetxt("test.csv", tmp_data, delimiter=';')
