d9eeae113462b49007f38cbe11c716233b0bba6b
steve <551619855@qq.com>
2017-08-04 23:29:03 +0800

    Some problem before calculating the matrix F.

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -276,6 +276,7 @@ int main() {
             if (std::isnan(C.sum())) {
                 std::cout << C << "\n" << "t: " << t << "\nline:"
                           << __FILE__ << " " << __LINE__ << std::endl;
+                std::cout << "acc_n : " << acc_n.block(t,0,1,3) << " acc i :" << acc_s.block(t,0,1,3) << std::endl;
                 std::cout << "vel_n x :" << vel_n.block(t, 0, 1, 3) << std::endl;
                 C = C_prev;
             }
