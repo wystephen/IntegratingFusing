32cc5d802662aa335bc06fd033f0077993d623a9
steve <551619855@qq.com>
2017-08-05 09:29:58 +0800

    Some problem before calculating the matrix F.

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -152,15 +152,22 @@ int main() {
         //Skew-symmetric matrix for angular rates
 
         Eigen::Vector3d gyro_s1 = gyro_s.block(t, 0, 1, 3).transpose();
-
         Eigen::Matrix3d ang_rate_matrix;
+        ang_rate_matrix.setZero();
+        if(gyro_s1.norm() > 1e-8)
+        {
+
+
 
         ang_rate_matrix << 0.0f, -gyro_s(2), gyro_s1(1),
                 gyro_s1(2), 0.0f, -gyro_s1(0),
                 -gyro_s1(1), gyro_s1(0), 0.0f;
 
-        C = C * ((2 * Eigen::Matrix3d::Identity() + (dt * ang_rate_matrix)) *
+        C = C * ((2.0 * Eigen::Matrix3d::Identity() + (dt * ang_rate_matrix)) *
                  (2.0 * Eigen::Matrix3d::Identity() - (dt * ang_rate_matrix)).inverse());
+        }
+
+
         if (std::isnan(C.sum()) || (C * C.transpose()).norm() > 3.2) {
             std::cout << "t: " << t
                       << "\n ang rate : " << ang_rate_matrix
