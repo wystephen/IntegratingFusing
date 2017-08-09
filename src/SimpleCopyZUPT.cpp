962d5d679270638fbc4221b50ed92fcd8cc726e3
steve <551619855@qq.com>
2017-08-09 19:43:25 +0800

    added a low bound threold for ang matrix....

--- src/SimpleCopyZUPT.cpp
+++ src/SimpleCopyZUPT.cpp
@@ -215,7 +215,6 @@ int main() {
             std::cout << "t: " << t
                       << "\nC : " << C
                       << "\n C*C^T:" << C * C.transpose()
-
                       << " \nacc_n: " << acc_n.block(t, 0, 1, 3)
                       << "\n acc_s : " << acc_s.block(t, 0, 1, 3) << std::endl
                       << std::endl;
